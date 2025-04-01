64ba8dfed1a92b500013ad14b9c8dcec90caea09
Doniele Arys Antonio <arysantonio363@gmail.com>
2025-04-01 17:40:45 +0800

    chore: delete test.r

--- test.r
+++
@@ -1,8 +0,0 @@
-library(ggplot2)
-data(iris)
-iris_scaled <- scale(iris[,1:4])
-set.seed(123)
-kmeans_result <- kmeans(iris_scaled, centers = 3)
-iris$Cluster <- as.factor(kmeans_result$cluster)
-ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Cluster)) + 
-  geom_point() + theme_minimal() + labs(title = "K-Means Clustering of Iris Data")
